2503d795ba0d22967a9cc51ff4b2b6da0c1e93cc
Arkadiusz Dutka <arcadio.dutka@hotmail.com>
2022-06-12 23:15:49 +0200

    Question Controller

---
+++ Aromat.Quiz.Api/Aromat.Quiz.Api/Controllers/QuestionsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aromat.Quiz.Api.Controllers
+{
+    public class QuestionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult CreateQuestion([FromBody] CreateQuestionDto dto)
+        {
+
+        }
+    }
+
+
+}
