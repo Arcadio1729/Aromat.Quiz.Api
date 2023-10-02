4549f71fe134850f16772eaa86d661507bf4aff3
ADU\ADutka <arkadiusz.dutka@ibcs.pl>
2023-10-02 12:19:37 +0200

    cleaned

--- Aromat.Upload.Api/Aromat.Upload.Api/Model/UploadDbContext.cs
+++ Aromat.Upload.Api/Aromat.Upload.Api/Model/UploadDbContext.cs
@@ -8,9 +8,6 @@ namespace Aromat.Upload.Api.Model
 {
     public class UploadDbContext : DbContext
     {
-        //private readonly string _connectionString = @"Server=tcp:aromat-db-server.database.windows.net,1433;Initial Catalog=AromatDb;Persist Security Info=False;User ID=aromat-admin;Password=Jaromir#68;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-        //private readonly string _connectionString = "Server=Localhost;User=LAPTOP-FAABQ3F4\\arcad;Database=Aromat;Trusted_Connection=True;";
-        //private readonly string _connectionString = "Server=ADU;User=arcadio;Database=Aromat;Password=pass1;Trusted_Connection=True;";
 
         public DbSet<FileData> FileData { get; set; }
 
